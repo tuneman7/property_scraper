25f9072f6afc40a5490113552d794d0bf0e5c19e
Irwin <tuneman7@hotmail.com>
2021-06-23 23:49:38 -0700

    dealio

--- project_1_main.py
+++ project_1_main.py
@@ -7,11 +7,13 @@ from project_1_objects import  AreaInformationByZipcode
 
 def main():
 
-    # area_data_store = AreaDataStore()
+    area_data_store = AreaDataStore()
+    #area_data_store.smooth_data()
     # l_areas_by_zipcode = area_data_store.get_area_info_by_zipcode('90068')
     # for object in l_areas_by_zipcode:
     #     print(object.zipcode,object.extract_day_id)
     # # print(area_data_store.area_data_objects_by_zipcode)
+
 
 
     # area_graph = AreaGraph()
