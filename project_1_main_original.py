78bafdf8407485bd651fb8cb32ef01f6095398ad
Irwin <tuneman7@hotmail.com>
2021-06-24 19:17:56 -0700

    routine push

--- project_1_main_original.py
+++ project_1_main_original.py
@@ -1,8 +1,27 @@
+import project_1_objects
+from project_1_objects import AreaDataStore
 from project_1_objects import AreaDataMenu
 from project_1_objects import AreaDisplay
+from project_1_objects import AreaGraph
+from project_1_objects import  AreaInformationByZipcode
 from project_1_objects import Utility
-
 def main():
+
+    # import matplotlib
+    # print('matplotlib: {}'.format(matplotlib.__version__))
+    # return
+
+    # area_data_store = AreaDataStore()
+    # area_data_store.smooth_data()
+    # l_areas_by_zipcode = area_data_store.get_area_info_by_zipcode('90068')
+    # for object in l_areas_by_zipcode:
+    #     print(object.zipcode,object.extract_day_id)
+    # # print(area_data_store.area_data_objects_by_zipcode)
+
+
+
+    # area_graph = AreaGraph()
+    # area_graph.plot_the_dealio()
 
     #This will exit us out if we're not cool
     util = Utility()
@@ -15,23 +34,6 @@ def main():
     while stil_running:
 
         area_data_menu.display_area_data_menu()
-        zip_code_to_display = area_data_menu.get_main_menu_input()
-        area_display_object = AreaDisplay(zip_code_to_display)
-
-        return_to_main_menu = False
-        while not return_to_main_menu:
-            area_display_object.display_area_data_menu()
-            area_display_option = area_display_object.get_area_menu_input()
-
-            if area_display_option[0] == 'return':
-                return_to_main_menu = True
-                break
-            else:
-                # print(area_display_option)
-                area_display_object.display_graph_based_on_input(area_display_option,zip_code_to_display)
-
-main()
-a_menu()
         zip_code_to_display = area_data_menu.get_main_menu_input()
         area_display_object = AreaDisplay(zip_code_to_display)
 
